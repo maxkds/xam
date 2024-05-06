89093264f6a4c883ad94d6e3f9d542c9ecfd5e55
maxkds <saliamit82@gmail.com>
2024-05-06 20:50:04 +0530

    Sigmoid.py

--- 1-Sigmoid.py
+++ 1-Sigmoid.py
@@ -38,3 +38,21 @@ plt.suptitle('ACTIVATION FUNCTIONS')
 
 plt.show()
 
+
+"""  
+import numpy as np
+import matplotlib.pyplot as plt
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+x= np.linspace(-10,10,100)
+
+y=sigmoid(x)
+
+plt.title("sigmoid")
+plt.plot(x,y)
+
+
+"""
+
